33c5020f3a71f4f4e63cfe6b71e12ae7a881e2fd
Lorenzo Giuliani <lorenzo@frenzart.com>
2017-01-22 10:52:16 +0100

    bspwm: volume with notifications

---
+++ bspwm/.config/bspwm/scripts/vol.sh
@@ -0,0 +1,20 @@
+#!/bin/dash
+set -e
+
+case "$1" in
+    "up")
+        pulseaudio-ctl up
+        ;;
+    "down")
+        pulseaudio-ctl down
+        ;;
+    "mute")
+        pulseaudio-ctl mute
+        ;;
+    *)
+        exit 1
+        ;;
+esac
+
+volume="$(pulseaudio-ctl full-status|sed 's|[^0-9]||g')"
+notify-send -u low -t 1500 "volume ${volume}%"
