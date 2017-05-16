f9d18a2dfde72bdbda7554435d54207e04a360d0
Lorenzo Giuliani <lorenzo@frenzart.com>
2017-05-16 16:00:38 +0200

    zsh: tmux plugin

--- shell/.zsh/init.zsh
+++ shell/.zsh/init.zsh
@@ -1,11 +1,5 @@
 source $HOME/.zsh/antigen.bundle.zsh
 
-# Gruvbox colors
-_GBOX="$HOME/.config/nvim/bundle/vim-gruvbox/gruvbox_256palette.sh"
-[ -f "$_GBOX" ] && source $_GBOX
-
-MINIMAL_RPROMPT=no
-
 # Config
 source $HOME/.zsh/completion.zsh
 source $HOME/.zsh/config.zsh
@@ -15,6 +9,7 @@ source $HOME/.zsh/utils.zsh
 source $HOME/.zsh/alias.zsh
 source $HOME/.zsh/nvm.zsh
 source $HOME/.zsh/keychain.zsh
+source $HOME/.zsh/tmux.zsh
 
 if [ "$ZSH_THEME" = "delta" ]; then
   source $HOME/.zsh/theme.delta.zsh
--- shell/.zsh/tmux.zsh
+++ shell/.zsh/tmux.zsh
@@ -1,5 +1,17 @@
 
 # Start tmux at start
-if [ -z "$Z_TMUX" ]; then
+if [ -n "$Z_TMUX" ]; then
   test -z "$TMUX" && (tmux attach || tmux new-session)
+fi
+
+[ -z "$TMUX" ] && export TERM=xterm-256color
+
+if [ -n "$TMUX" ]; then
+  TMUX_SESSION=`tmux display-message -p '#S'`
+  SOCK="$HOME/.ssh/.ssh-agent-$USER-tmux-$TMUX_SESSION"
+  if test $SSH_AUTH_SOCK && [ $SSH_AUTH_SOCK != $SOCK ]; then
+    rm -f $SOCK
+    ln -sf $SSH_AUTH_SOCK $SOCK
+    export SSH_AUTH_SOCK=$SOCK
+  fi
 fi
