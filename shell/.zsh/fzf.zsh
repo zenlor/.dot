056f61cc9f956a47772bbef63230d9bf3e206899
Lorenzo Giuliani <lorenzo@frenzart.com>
2018-01-17 10:32:27 +0100

    shell/zsh: ^P as fzf file chooser

--- shell/.zsh/fzf.zsh
+++ shell/.zsh/fzf.zsh
@@ -66,4 +66,40 @@ if [ -x "$(which fzf)" ]; then
 
     zle     -N   fzfz-file-widget
     bindkey '^G' fzfz-file-widget
+
+    # # Ctrl-P - cd to dir or edit file with fzf
+    # Put in /usr/local/share/zsh/site-functions/
+    # and add to .zshrc
+    # source /usr/local/share/zsh/site-functions/ctrl-p
+    # inspired by
+    # https://adamheins.com/blog/ctrl-p-in-the-terminal-with-fzf
+    fzf-edit-file-or-open-dir() {
+        local out key file helpline
+        helpline="Ctrl-f to reveal in Finder | Enter to edit file"
+        # IFS=$'\n' out=($(ag -g "" | fzf --header="$helpline" \
+            # IFS=$'\n' out=($(bfs | fzf --header="$helpline" \
+            IFS=$'\n' out=($(rg --files --hidden --glob "!.git/*" | fzf --header="$helpline" \
+            --exit-0 \
+            --expect=ctrl-f \
+            --preview '[ -f {} ] && head -n 50 {}' \
+            --preview-window down:4 \
+            --bind='?:toggle-preview' ))
+        key=$(head -1 <<< "$out")
+        file=$(head -2 <<< "$out" | tail -1)
+
+        if [ "$key" = ctrl-f ]; then
+            open -R "$file" # reveal in Finder
+        else
+            if [ -f "$file" ]; then
+                # /usr/local/bin/mvim "$file"
+                /usr/local/bin/mvim "$file"
+            elif [ -d "$file" ]; then
+                cd "$file"
+            fi
+            zle reset-prompt
+        fi
+        zle accept-line
+    }
+    zle     -N   fzf-edit-file-or-open-dir
+    bindkey '^P' fzf-edit-file-or-open-dir
 fi
