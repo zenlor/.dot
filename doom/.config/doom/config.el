0963e855923887bf75dfff2c3886899a786292a0
Lorenzo Giuliani <lorenzo@frenzart.com>
2018-02-21 17:25:31 +0100

    doom: email settings

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -1,27 +1,47 @@
 ;;; private/cli/config.el -*- lexical-binding: t; -*-
 
+(defvar xdg-data (getenv "XDG_DATA_HOME"))
+(defvar xdg-bin (getenv "XDG_BIN_HOME"))
+(defvar xdg-cache (getenv "XDG_CACHE_HOME"))
+(defvar xdg-config (getenv "XDG_CONFIG_HOME"))
+
 ;; app/email
-(pcase (system-name)
-       ("cli.mb") ;; (or "cli.mb" "meila")
-       (after! 'mu4e
-               (setq smtpmail-stream-type 'starttls
-                     smtpmail-default-smtp-server "smtp.office365.com"
-                     smtpmail-smtp-server "smtp.office365.com"
-                     smtpmail-smtp-service 587)
+(after! mu4e
+  (setq mu4e-bookmarks
+        `(("\\\\Inbox" "Inbox" ?i)
+          ("\\\\Draft" "Drafts" ?d)
+          ("flag:unread AND \\\\Inbox" "Unread messages" ?u)
+          ("flag:flagged" "Starred messages" ?s)
+          ("date:today..now" "Today's messages" ?t)
+          ("date:7d..now" "Last 7 days" ?w)
+          ("mime:image/*" "Messages with images" ?p)))
 
-               (set! :email "outlook"
-                 '((mu4e-sent-folder       . "/Sent Items")
-                   (mu4e-drafts-folder     . "/Drafts")
-                   (mu4e-trash-folder      . "/Trash")
-                   (mu4e-refile-folder     . "/INBOX")
-                   (smtpmail-smtp-user     . "lorenzo@mobilebridge.com")
-                   (user-mail-address      . "lorenzo@mobilebridge.com")
-                   (mu4e-compose-signature . "---\nLorenzo")))))
+  (setq smtpmail-stream-type 'starttls
+        smtpmail-default-smtp-server "smtp.gmail.com"
+        smtpmail-smtp-server "smtp.gmail.com"
+        smtpmail-smtp-service 587)
 
-;; (setq doom-theme 'doom-molokai)
-;; (setq doom-theme 'doom-solarized-light)
+  (set! :email "gmail.com"
+    '((mu4e-sent-folder       . "/aliem13@gmail.com/Sent Mail")
+      (mu4e-drafts-folder     . "/aliem13@gmail.com/Drafts")
+      (mu4e-trash-folder      . "/aliem13@gmail.com/Trash")
+      (mu4e-refile-folder     . "/aliem13@gmail.com/All Mail")
+      (smtpmail-smtp-user     . "aliem13@gmail.com")
+      (user-mail-address      . "aliem13@gmail.com")
+      (mu4e-compose-signature . "---\nLor")))
+
+  (set! :email "frenzart.com"
+    '((mu4e-sent-folder       . "/lorenzo@frenzart.com/Sent Mail")
+      (mu4e-drafts-folder     . "/lorenzo@frenzart.com/Drafts")
+      (mu4e-trash-folder      . "/lorenzo@frenzart.com/Trash")
+      (mu4e-refile-folder     . "/lorenzo@frenzart.com/All Mail")
+      (smtpmail-smtp-user     . "lorenzo@frenzart")
+      (user-mail-address      . "lorenzo@frenzart")
+      (mu4e-compose-signature . "---\nLorenzo Giuliani"))
+    t))
+
+;; theme
 (setq doom-theme 'doom-tomorrow-night)
-
 
 ;; host-specific settings
 (pcase (system-name)
