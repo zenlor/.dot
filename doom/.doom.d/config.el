a0726c8086fc069ae17a9d7364567b502c2662bf
Lorenzo Giuliani <lorenzo@frenzart.com>
2018-04-04 12:59:07 +0200

    doom: update to last develop

--- doom/.doom.d/config.el
+++ doom/.doom.d/config.el
@@ -72,3 +72,28 @@
       :n "C-j"   #'evil-window-down
       :n "C-k"   #'evil-window-up
       :n "C-l"   #'evil-window-right)
+;; host-specific settings
+(load "~/.emacs.local.el" t t)
+
+(setq user-mail-address "lorenzo@giuliani.me"
+      user-full-name    "Lorenzo Giuliani")
+
+;; JS2-Mode
+(with-eval-after-load "js2-mode"
+  (setq-default
+    js2-ignored-warnings '("msg.return.inconsistent" "msg.anon.no.return.value" "msg.no.return.value" "msg.no.side.effects"))
+  (setq js2-strict-trailing-comma-warning nil
+        js2-strict-missing-semi-warning nil
+        js2-strict-code-has-no-side-effects nil
+        js2-missing-semi-one-line-override nil))
+
+
+;; Tide/TypeScript
+(setq company-tooltip-align-annotations t)
+(setq tide-tsserver-executable "node_modules/typescript/bin/tsserver")
+
+(add-hook 'typescript-mode-hook
+          (lambda ()
+            (local-set-key (kbd "M-j") 'c-indent-new-comment-line)
+            (local-set-key (kbd "RET") 'c-indent-new-comment-line)
+            (add-hook 'before-save-hook 'tide-format-before-save)))
--- doom/.doom.d/init.el
+++ doom/.doom.d/init.el
@@ -1,33 +1,4 @@
-;;; init.el -*- lexical-binding: t; -*-
-;;
-;; Author:  Henrik Lissner <henrik@lissner.net>
-;; URL:     https://github.com/hlissner/doom-emacs
-;;
-;;   =================     ===============     ===============   ========  ========
-;;   \\ . . . . . . .\\   //. . . . . . .\\   //. . . . . . .\\  \\. . .\\// . . //
-;;   ||. . ._____. . .|| ||. . ._____. . .|| ||. . ._____. . .|| || . . .\/ . . .||
-;;   || . .||   ||. . || || . .||   ||. . || || . .||   ||. . || ||. . . . . . . ||
-;;   ||. . ||   || . .|| ||. . ||   || . .|| ||. . ||   || . .|| || . | . . . . .||
-;;   || . .||   ||. _-|| ||-_ .||   ||. . || || . .||   ||. _-|| ||-_.|\ . . . . ||
-;;   ||. . ||   ||-'  || ||  `-||   || . .|| ||. . ||   ||-'  || ||  `|\_ . .|. .||
-;;   || . _||   ||    || ||    ||   ||_ . || || . _||   ||    || ||   |\ `-_/| . ||
-;;   ||_-' ||  .|/    || ||    \|.  || `-_|| ||_-' ||  .|/    || ||   | \  / |-_.||
-;;   ||    ||_-'      || ||      `-_||    || ||    ||_-'      || ||   | \  / |  `||
-;;   ||    `'         || ||         `'    || ||    `'         || ||   | \  / |   ||
-;;   ||            .===' `===.         .==='.`===.         .===' /==. |  \/  |   ||
-;;   ||         .=='   \_|-_ `===. .==='   _|_   `===. .===' _-|/   `==  \/  |   ||
-;;   ||      .=='    _-'    `-_  `='    _-'   `-_    `='  _-'   `-_  /|  \/  |   ||
-;;   ||   .=='    _-'          '-__\._-'         '-_./__-'         `' |. /|  |   ||
-;;   ||.=='    _-'                                                     `' |  /==.||
-;;   =='    _-'                                                            \/   `==
-;;   \   _-'                                                                `-_   /
-;;    `''                                                                      ``'
-;;
-;; These demons are not part of GNU Emacs.
-;;
-;;; License: MIT
-
-(require 'core (concat user-emacs-directory "core/core"))
+;;; init.example.el -*- lexical-binding: t; -*-
 
 (doom! :feature
        (popup            ; tame sudden yet inevitable temporary windows
@@ -107,6 +78,7 @@
        ledger            ; an accounting system in Emacs
        lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
+       nix               ; I hereby declare "nix geht mehr!"
        ocaml             ; an objective camel
        (org              ; organize your plain life in plain text
         +attach          ; custom attachment system
@@ -146,9 +118,5 @@
        ;; a Spacemacs-inspired keybinding scheme, a custom yasnippet library,
        ;; and additional ex commands for evil-mode. Use it as a reference for
        ;; your own modules.
-       (default +bindings +snippets +evil-commands)
+       (default +bindings +snippets +evil-commands))
 
-       ;; This allows you to store your private module at
-       ;; $XDG_CONFIG_HOME/doom/. Without +xdg it uses ~/.doom.d/. If your
-       ;; config directory doesn't exist, this module does nothing.
-       (private +xdg))
