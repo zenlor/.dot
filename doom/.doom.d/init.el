ae9014391edb514c318e8c3b809c5004b19d75a6
Lorenzo Giuliani <lorenzo@frenzart.com>
2018-04-10 17:06:25 +0200

    doom: disable unstable evil settings

--- doom/.doom.d/init.el
+++ doom/.doom.d/init.el
@@ -6,7 +6,8 @@
         +defaults)       ; default popup rules
       ;debugger          ; FIXME stepping through code, to help you add bugs
        eval              ; run code, run (also, repls)
-       (evil +everywhere); come to the dark side, we have cookies
+       evil
+      ;(evil +everywhere); come to the dark side, we have cookies
        file-templates    ; auto-snippets for empty files
        (lookup           ; helps you navigate your code and documentation
         +devdocs         ; ...on devdocs.io online
