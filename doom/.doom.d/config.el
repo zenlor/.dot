6feb3fdae4270c0b6a417e3100a00665e457be57
Lorenzo Giuliani <lorenzo@frenzart.com>
2018-08-01 10:25:21 +0200

    doom: Carp config

--- doom/.doom.d/config.el
+++ doom/.doom.d/config.el
@@ -117,3 +117,15 @@
   (set! :company-backend 'systemd-mode '(systemd-mode-hook)))
 
 (require 'evil-mu4e)
+
+;;
+;; Carp Lang
+;;
+(add-to-list 'load-path "~/lib/src/github.com/carp-lang/Carp/emacs")
+(def-package! carp-mode
+  :mode "\\.carp$"
+  :init)
+
+(def-package! inf-carp-mode
+  :after 'carp-mode)
+
