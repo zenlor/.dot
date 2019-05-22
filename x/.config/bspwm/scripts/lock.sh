5c7258e4e1a01fd5f45d56ea295260982a6af6c5
Lorenzo Giuliani <lorenzo@frenzart.com>
2019-05-22 17:16:21 +0300

    x: remove bashism

--- x/.config/bspwm/scripts/lock.sh
+++ x/.config/bspwm/scripts/lock.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-if [ command -v cowfortune >/dev/null 2>&1 ]; then
+if command -v cowfortune >/dev/null 2>&1; then
     exec slock -m "`cowfortune`"
 else
-    exec slock -m "${USER}@${HOST}"
+    exec slock -m "${USER}@${HOSTNAME}"
 fi
