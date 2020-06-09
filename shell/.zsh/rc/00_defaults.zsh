8dddbb2f149f8cfc412bd26251b4d1b98f98f589
Lorenzo Giuliani <lgiuliani@malwarebytes.com>
2020-06-09 16:29:58 +0200

    shell: zsh smarter up/down

--- shell/.zsh/rc/00_defaults.zsh
+++ shell/.zsh/rc/00_defaults.zsh
@@ -68,3 +68,14 @@
     setopt share_history
 # }}}
 
+## Up/Down history search with context {{{
+    autoload -Uz up-line-or-beginning-search down-line-or-beginning-search
+
+    zle -N up-line-or-beginning-search
+    zle -N down-line-or-beginning-search
+
+    bindkey '^[[A'  up-line-or-beginning-search    # Arrow up
+    bindkey '^[OA'  up-line-or-beginning-search
+    bindkey '^[[B'  down-line-or-beginning-search  # Arrow down
+    bindkey '^[OB'  down-line-or-beginning-search#
+#}}}
