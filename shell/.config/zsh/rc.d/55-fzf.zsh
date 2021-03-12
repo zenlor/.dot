0a4af5e85d11cbacbbcba7c146485a30966768d6
Lorenzo Giuliani <lgiuliani@malwarebytes.com>
2021-03-12 12:19:48 +0100

    shell: fixup things

---
+++ shell/.config/zsh/rc.d/55-fzf.zsh
@@ -0,0 +1,32 @@
+#!/bin/zsh
+
+if [[ $- == *i* ]]; then
+    if type 'fzf' > /dev/null; then
+
+        if [[ -f ${HOME}/.nix-profile/share/fzf/key-bindings.zsh ]]; then
+            FZF_SHARE="${HOME}/.nix-profile/share/fzf"
+        else
+            if [[ ! -d ${HOME}/.cache/fzf ]]; then
+                git clone --depth 1 https://github.com/junegunn/fzf.git ${HOME}/.cache/fzf
+            fi
+
+            FZF_SHARE="${HOME}/.cache/fzf/shell"
+        fi
+
+        source "${FZF_SHARE}/completion.zsh" 2> /dev/null
+        source "${FZF_SHARE}/key-bindings.zsh"
+
+
+        export FZF_DEFAULT_OPS='--extended'
+        export FZF_CTRL_T_COMMAND="${FZF_DEFAULT_COMMAND}"
+
+        # If fd command is installed, use it instead of find
+        if type 'fd' > /dev/null; then
+            export FZF_DEFAULT_COMMAND='fd --type f'
+        fi
+
+        # if [[ -d ~/.cache/fzf/man ]]; then
+        #     export MANPATH="$MANPATH:~/.cache/fzf/man"
+        # fi
+    fi
+fi
---
+++ shell/.config/zsh/rc.d/99-config.zsh
@@ -0,0 +1,79 @@
+setopt autocd
+
+# Yes, these are a pain to customize. Fortunately, Geoff Greer made an online
+# tool that makes it easy to customize your color scheme and keep them in sync
+# across Linux and OS X/*BSD at http://geoff.greer.fm/lscolors/
+export CLICOLOR=1
+export LSCOLORS='Exfxcxdxbxegedabagacad'
+export LS_COLORS='di=1;34;40:ln=35;40:so=32;40:pi=33;40:ex=31;40:bd=34;46:cd=34;43:su=0;41:sg=0;46:tw=0;42:ow=0;43:'
+
+export COMPLETION_WAITING_DOTS="true"
+# Correct spelling for commands
+setopt correct
+# turn off the infernal correctall for filenames
+unsetopt correctall
+
+zstyle ':completion:*' matcher-list 'r:[[:ascii:]]||[[:ascii:]]=** r:|=* m:{a-z\-}={A-Z\_}'
+
+# set some history options
+setopt append_history
+setopt extended_history
+setopt hist_expire_dups_first
+setopt hist_ignore_all_dups
+setopt hist_ignore_dups
+setopt hist_ignore_space
+setopt hist_reduce_blanks
+setopt hist_save_no_dups
+setopt hist_verify
+setopt INC_APPEND_HISTORY
+unsetopt HIST_BEEP
+
+# Share your history across all your terminal windows
+setopt share_history
+setopt noclobber
+
+# Keep a ton of history. You can reset these without editing .zshrc by
+# adding a file to ~/.zshrc.d.
+HISTSIZE=100000
+SAVEHIST=100000
+HISTFILE=~/.zsh_history
+export HISTIGNORE="ls:cd:cd -:pwd:exit:date:* --help"
+
+# set some options about directories
+setopt pushd_ignore_dups
+#setopt pushd_silent
+setopt AUTO_CD  # If a command is issued that can’t be executed as a normal command,
+# and the command is the name of a directory, perform the cd command
+# to that directory.
+
+# Add some completions settings
+setopt ALWAYS_TO_END     # Move cursor to the end of a completed word.
+setopt AUTO_LIST         # Automatically list choices on ambiguous completion.
+setopt AUTO_MENU         # Show completion menu on a successive tab press.
+setopt AUTO_PARAM_SLASH  # If completed parameter is a directory, add a trailing slash.
+setopt COMPLETE_IN_WORD  # Complete from both ends of a word.
+unsetopt MENU_COMPLETE   # Do not autoselect the first completion entry.
+
+# Miscellaneous settings
+setopt INTERACTIVE_COMMENTS  # Enable comments in interactive shell.
+
+# Long running processes should return time after they complete. Specified
+# in seconds.
+REPORTTIME=2
+TIMEFMT="%U user %S system %P cpu %*Es total"
+
+# Expand aliases inline - see http://blog.patshead.com/2012/11/automatically-expaning-zsh-global-aliases---simplified.html
+globalias() {
+    if [[ $LBUFFER =~ ' [A-Z0-9]+$' ]]; then
+        zle _expand_alias
+        zle expand-word
+    fi
+    zle self-insert
+}
+
+zle -N globalias
+
+bindkey " " globalias
+bindkey "^ " magic-space           # control-space to bypass completion
+bindkey -M isearch " " magic-space # normal space during searches
+
