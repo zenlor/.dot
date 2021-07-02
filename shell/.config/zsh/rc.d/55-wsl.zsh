1dc3db33b8a886fa1b6bb287ea8cc0f22114f8d7
Lorenzo Giuliani <lorenzo@frenzart.com>
2021-07-02 18:49:11 +0200

    zsh: support WSLg

--- shell/.config/zsh/rc.d/55-wsl.zsh
+++ shell/.config/zsh/rc.d/55-wsl.zsh
@@ -1,12 +1,14 @@
 if uname -r|grep -q microsoft;then
-    if command -v route >/dev/null; then
-        WSL_IP="$(route -n|grep 0.0.0.0|awk '{print $2;exit;}')"
-    else
-        WSL_IP="$(ip route|grep 'default'|awk '{print $3;exit;}')"
-    fi
-    export DISPLAY="${WSL_IP}:0.0"
-    export KEYMAP=/etc/keymap/us.bmap.gz
-    export LIBGL_ALWAYS_INDIRECT=1
+    if [[ ! -v WAYLAND_DISPLAY ]]; then
+        if command -v route >/dev/null; then
+            WSL_IP="$(route -n|grep 0.0.0.0|awk '{print $2;exit;}')"
+        else
+            WSL_IP="$(ip route|grep 'default'|awk '{print $3;exit;}')"
+        fi
+        export DISPLAY="${WSL_IP}:0.0"
+        export KEYMAP=/etc/keymap/us.bmap.gz
+        export LIBGL_ALWAYS_INDIRECT=1
 
-   export PULSE_SERVER="tcp:${WSL_IP}"
+        export PULSE_SERVER="tcp:${WSL_IP}"
+    fi
 fi
