b10eaf3b6e0274a8f8661fd21da998bcd3ae444c
Lorenzo Giuliani <lgiuliani@malwarebytes.com>
2022-10-23 03:48:54 +0200

    doom: lsp with eglot

--- doom/.config/doom/init.el
+++ doom/.config/doom/init.el
@@ -92,9 +92,10 @@
        editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
-       ;;gist              ; interacting with github gists
+       gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
        (lsp +eglot)               ; M-x vscode
+       ;;lsp
        magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
